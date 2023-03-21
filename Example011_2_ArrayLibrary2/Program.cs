3c33728546ff60d023c3fd6303e2e40575cd56a4
mmickoff <mickmail2000@gmail.com>
2023-03-21 18:53:05 +0300

    Created Ex 10-12_00

---
+++ Example011_2_ArrayLibrary2/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        // System.Console.Write(collection[index] + " ");
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int i = 0;
+    while (i < count)
+    {
+        System.Console.Write(col[i] + " ");
+        i++;
+    }
+    System.Console.WriteLine();
+}
+
+int IndexOff(int[] collect, int find)
+{
+    int index = 0;
+    int count = collect.Length;
+    int position = -1;
+    while (index < count)
+    {
+        if (collect[index] == find)
+        {
+            position = index;
+            break;
+        }
+        // return position;}
+        // else if (collect[index] != find && index == count - 1)
+        // position = -1;
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine(IndexOff(array, 6));
---
+++ Example012_000_Exp_01/Program.cs
@@ -0,0 +1,31 @@
+﻿// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count){
+// Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21(count: 8, msg: "Новое сообщение");
+
+int Method3()
+    {
+return DateTime.Now.Year;
+    }
+
+int date = Method3();
+// System.Console.WriteLine(date);
+
+string Method4(int count, string text)
+{
+    int i=0;
+    string result = String.Empty; // что означает ""; пустая строка;
+    while(i <count){
+result = result  + text;
+        i++;
+    }
+    return result;
+}
+
+string  txt = Method4(text: "Ура! ", count: 6);
+System.Console.WriteLine(txt);
